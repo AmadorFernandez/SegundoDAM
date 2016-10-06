dc2a9d0ce838fc3933e74e0273a197886d27dd0d
Amador <amadorfernandez2@gmail.com>
2016-10-06 22:49:54 +0200

    Subida desde casa

---
+++ PMDMO/New Unity Project/Assets/Script/MoverBola.cs
@@ -0,0 +1,54 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MoverBola : MonoBehaviour {
+
+
+    float velocidad = 2f;
+    public float altura = 8F;
+    Rigidbody rb;
+
+    void Start()
+    {
+
+         rb = GetComponent<Rigidbody>();
+
+    }
+
+	// Update is called once per frame
+	void Update () {
+
+        float vGiro = 30f;
+        transform.Translate(0,0,Input.GetAxis("Profundidad") * Time.deltaTime * velocidad);
+        transform.Translate(Input.GetAxis("Horizontal") * Time.deltaTime * velocidad,0, 0 );
+        //    transform.Rotate(0,0 , Input.GetAxis("Horizontal") * Time.deltaTime * vGiro*-1);
+        
+
+        if(Input.GetAxis("Profundidad") != 0)
+        {
+
+           
+
+        }
+
+        if (Input.GetKey(KeyCode.Space) &&  transform.position.y <= 0.59)
+        {
+            rb.AddForce(new Vector3(0, altura, 0), ForceMode.Impulse);
+
+        }
+     
+
+        
+
+
+
+
+
+
+
+
+
+
+
+    }
+}
---
+++ PMDMO/New Unity Project/Assets/Script/SeguirBola.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SeguirBola : MonoBehaviour {
+
+
+    public GameObject bola;
+    Vector3 distancia;
+
+	// Use this for initialization
+	void Start () {
+
+        distancia = transform.position;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        transform.position = new Vector3(bola.transform.position.x, distancia.y+bola.transform.position.y, bola.transform.position.z+distancia.z);
+
+    }
+}
